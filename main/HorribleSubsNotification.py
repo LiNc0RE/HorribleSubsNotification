ccb29da2a83047adcf3c1aa0938e3586d57bd456
Daniel Müller <lincore@hotmail.de>
2016-07-18 18:45:32 +0200

    script added
    very little script added. Notifies of newly published anime episodes,
    that I want to watch

---
+++ main/HorribleSubsNotification.py
@@ -0,0 +1,66 @@
+from win32api import *
+from win32gui import *
+import win32con
+import sys, os
+import struct
+import time
+import feedparser
+
+sub_group="HorribleSubs"
+quality="720"
+whitelist=["Orange","DAYS","One Piece"]
+
+class WindowsBalloonTip:
+    def __init__(self, title, msg):
+        message_map = {
+                win32con.WM_DESTROY: self.OnDestroy,
+        }
+        # Register the Window class.
+        wc = WNDCLASS()
+        hinst = wc.hInstance = GetModuleHandle(None)
+        wc.lpszClassName = "PythonTaskbar"
+        wc.lpfnWndProc = message_map # could also specify a wndproc.
+        classAtom = RegisterClass(wc)
+        # Create the Window.
+        style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
+        self.hwnd = CreateWindow( classAtom, "Taskbar", style, \
+                0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, \
+                0, 0, hinst, None)
+        UpdateWindow(self.hwnd)
+        iconPathName = os.path.abspath(os.path.join( sys.path[0], "balloontip.ico" ))
+        icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
+        try:
+           hicon = LoadImage(hinst, iconPathName, \
+                    win32con.IMAGE_ICON, 0, 0, icon_flags)
+        except:
+          hicon = LoadIcon(0, win32con.IDI_APPLICATION)
+        flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
+        nid = (self.hwnd, 0, flags, win32con.WM_USER+20, hicon, "tooltip")
+        Shell_NotifyIcon(NIM_ADD, nid)
+        Shell_NotifyIcon(NIM_MODIFY, \
+                         (self.hwnd, 0, NIF_INFO, win32con.WM_USER+20,\
+                          hicon, "Balloon  tooltip",msg,200,title))
+        #self.show_balloon(title, msg)
+        time.sleep(10)
+        DestroyWindow(self.hwnd)
+    def OnDestroy(self, hwnd, msg, wparam, lparam):
+        nid = (self.hwnd, 0)
+        Shell_NotifyIcon(NIM_DELETE, nid)
+        PostQuitMessage(0) # Terminate the app.
+
+def balloon_tip(title, msg):
+    w=WindowsBalloonTip(title, msg)
+    
+def getLatest():
+    feed = feedparser.parse('http://www.nyaa.se/?page=rss')
+    items = feed.entries
+    for item in items:
+        for listitem in whitelist:
+            if listitem in item.title and sub_group in item.title and quality in item.title:
+                balloon_tip("New Episode Available!", item.title.encode('utf-8'))
+    
+    
+while 1:
+    getLatest();
+    time.sleep(180);
+    
